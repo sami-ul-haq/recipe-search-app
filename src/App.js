7c9dbac3ab522c8a4b9fa93571985b86ca87235f
sami-ul-haq <samiulhaq1899@gmail.com>
2020-11-06 21:59:09 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Recipe from "./components/Recipe";
 
-function App() {
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "2px 4px",
+    margin: "10px auto",
+    display: "flex",
+    alignItems: "center",
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  const APP_ID = "9191e1b8";
+  const APP_KEY = "4f7bd66aec1f63fc0f3d3fe6f96023d7";
+
+  const [recipes, setRecipes] = useState([]);
+
+  const [search, setSearch] = useState("");
+
+  const [query, setQuery] = useState("chicken");
+
+  useEffect(() => {
+    getRecipe();
+  }, [query]);
+
+  const getRecipe = async () => {
+    const response = await axios.get(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    setRecipes(response.data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const updateQuery = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Paper component="form" onSubmit={updateQuery} className={classes.root}>
+        <InputBase
+          type="text"
+          value={search}
+          onChange={updateSearch}
+          className={classes.input}
+          placeholder="Search For Recipe"
+          inputProps={{ "aria-label": "search for recipe" }}
+        />
+        <IconButton
+          type="submit"
+          className={classes.iconButton}
+          aria-label="search"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      {/* Material UI Used Inplace Of This*/}
+      {/* <form onSubmit={updateQuery}>
+        <input type="text" value={search} onChange={updateSearch} />
+        <button type="submit">Search</button>
+      </form> */}
+      <div style={{ padding: 30 }}>
+        <Grid container spacing={3}>
+          {recipes.map((recipe) => (
+            <Grid item xs={4}>
+              <Recipe
+                title={recipe.recipe.label}
+                calories={recipe.recipe.calories}
+                image={recipe.recipe.image}
+                ingredient={recipe.recipe.ingredients}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+}));
+
+function Recipe({ title, calories, image, ingredient }) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <>
+      <Card className={classes.root}>
+        <CardHeader title={title} subheader={calories} />
+        <CardMedia className={classes.media} image={image} title={title} />
+
+        <CardActions disableSpacing>
+          <Typography>Ingredients</Typography>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            {ingredient.map((item, index) => (
+              <Typography paragraph>{item.text}</Typography>
+            ))}
+          </CardContent>
+        </Collapse>
+      </Card>
+
+      {/* We Used Material UI Card Inplace Of This */}
+      {/* <p>{title}</p>
+      <p>{calories}</p>
+      <img src={image} alt="images" />
+      <ol>
+        {ingredient.map((item, index) => (
+          <li key={index}>{item.text}</li>
+        ))}
+        ;
+      </ol> */}
+    </>
+  );
+}
+
+export default Recipe;
